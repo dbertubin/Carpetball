c3a0ae7d154d4cd028a58dbd68e4b095e8798d8a
Derek Bertubin <dbertubin@fullsail.edu>
2014-04-07 20:25:06 -0400

    Initial Commit

---
+++ Carpetball/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Carpetball
+//
+//  Created by Derek Bertubin on 4/7/14.
+//  Copyright Derek Bertubin 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ Carpetball/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Carpetball
+//
+//  Created by Derek Bertubin on 4/7/14.
+//  Copyright Derek Bertubin 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
